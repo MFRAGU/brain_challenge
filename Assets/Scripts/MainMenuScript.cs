622437cd69a25f8641170d770170ae006b986ad1
Karene01 <karenerakotonindrina@gmail.com>
2024-06-07 07:55:28 +0200

    main menu m-à-j

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
 
 public class MainMenuScript : MonoBehaviour 
 {
+    [SerializeField]
+    private Dropdown difficultyDropdown;
+    
+    [SerializeField]
+    private TableObject tableObject; 
 
    public void StartGame()
     {
@@ -12,5 +20,22 @@ public class MainMenuScript : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    private void Start()
+    {
+        // Initialiser le dropdown avec les valeurs de l'enum
+        difficultyDropdown.ClearOptions();
+        difficultyDropdown.AddOptions(System.Enum.GetNames(typeof(DifficultyLevel)).ToList());
+        difficultyDropdown.onValueChanged.AddListener(OnDifficultyChanged);
+
+        // Sélectionner la valeur par défaut
+        difficultyDropdown.value = (int)DifficultyLevel.Default;
+    }
+
+    private void OnDifficultyChanged(int index)
+    {
+        DifficultyLevel selectedDifficulty = (DifficultyLevel)index;
+        tableObject.DifficultyLevel = selectedDifficulty;
     }
 }
